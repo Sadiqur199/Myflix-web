9dce95e2597007645e506605cc14fa8f1ef69eed
Sadiqur199 <sadiqurrhaman199@gmail.com>
2023-06-26 05:04:23 +0600

    added banner and design

--- src/Components/Banner/Banner.jsx
+++ src/Components/Banner/Banner.jsx
@@ -1,9 +1,38 @@
-import React from 'react';
-
+import React, { useEffect, useState } from 'react';
+import request from '../../../src/Request'
+import axios from 'axios';
 const Banner = () => {
+  const [movies , setMovies] = useState([])
+  const movie = movies[Math.floor(Math.random()*movies.length)]
+  useEffect(()=>{
+    axios.get(request.requestPopular)
+    .then((res)=>{
+      setMovies(res.data.results)
+    })
+  },[])
+
+  console.log(movie)
+
   return (
-    <div>
-      <h1>this is banner</h1>
+    <div className='w-full h-[550px] text-white'>
+      <div className='w-full h-full'>
+        <div className='absolute w-full h-[550px] bg-gradient-to-r from-black'>
+
+        </div>
+        <img className='w-full h-full object-cover' src={`https://image.tmdb.org/t/p/original/${movie?.backdrop_path}`} alt={movie?.title}/>
+
+        <div className='absolute w-full top-[20%] p-4 md:p-8'>
+          <h1 className='text-3xl md:text-5xl font-bold'>{movie?.title}</h1>
+          <div className='my-4'>
+            <button className='border bg-gray-300 text-black border-gray-300 py-2 px-5'>
+              Play
+            </button>
+            <button className='border text-white border-gray-300 py-2 px-5 ml-4'>
+              Watch Later
+            </button>
+          </div>
+          </div>
+      </div>
     </div>
   );
 };
---
+++ src/Request.jsx
@@ -0,0 +1,10 @@
+const key='d270774e87a6482de14e8013be509849'
+
+const requests = {
+    requestPopular: `https://api.themoviedb.org/3/movie/popular?api_key=${key}&language=en-US&page=1`,
+    requestTrending: `https://api.themoviedb.org/3/movie/popular?api_key=${key}&language=en-US&page=2`,
+    requestHorror: `https://api.themoviedb.org/3/search/movie?api_key=${key}&language=en-US&query=horror&page=1&include_adult=false`,
+    requestUpcoming: `https://api.themoviedb.org/3/movie/upcoming?api_key=${key}&language=en-US&page=1`,
+  };
+
+  export default requests
