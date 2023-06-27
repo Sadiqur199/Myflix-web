f34e1fb599937e7a7e2a2e2821bcd9628d47dc5c
Sadiqur199 <sadiqurrhaman199@gmail.com>
2023-06-27 11:52:19 +0600

    categoryes movie added

--- src/Components/Banner/Banner.jsx
+++ src/Components/Banner/Banner.jsx
@@ -11,7 +11,16 @@ const Banner = () => {
     })
   },[])
 
-  console.log(movie)
+  // console.log(movie)
+
+  const truncateString = (str,num) =>{
+    if(str?.length>num){
+      return str.slice(0 , num ) + '...'
+    }
+    else {
+      return str
+    }
+  }
 
   return (
     <div className='w-full h-[550px] text-white'>
@@ -30,7 +39,9 @@ const Banner = () => {
             <button className='border text-white border-gray-300 py-2 px-5 ml-4'>
               Watch Later
             </button>
-          </div>
+          </div> 
+          <p className='text-gray-400 text-sm'>Released : {movie?.release_date}</p>
+          <p className='w-full md:max-w-[70%] lg:max-w-[50%] xl:max-w-[35%] text-gray-200'>{truncateString(movie?.overview , 200)}</p>
           </div>
       </div>
     </div>
--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,10 +1,18 @@
 import React from 'react';
 import Banner from '../Banner/Banner';
+import request from '../../../src/Request'
+import Row from '../Row/Row';
+
 
 const Home = () => {
   return (
     <div>
       <Banner></Banner>
+      <Row title = 'Upcoming Movie' fetchURL = {request.requestUpcoming}></Row>
+      <Row title = 'popular Movie' fetchURL = {request.requestPopular}></Row>
+      <Row title = 'Trending Movie' fetchURL = {request.requestTrending}></Row>
+      <Row title = 'TopRated Movie' fetchURL = {request.requestHorror}></Row>
+      <Row title = 'Horror Movie' fetchURL = {request.requestHorror}></Row>
     </div>
   );
 };
